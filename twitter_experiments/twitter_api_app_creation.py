6e520bcbc04f1e89b19aad4b0434fecb6b56ec95
merethrond <merethrond@gmail.com>
2018-07-11 16:35:12 +0530

    See whether login is working or not and report the status

--- twitter_experiments/twitter_api_app_creation.py
+++ twitter_experiments/twitter_api_app_creation.py
@@ -13,8 +13,13 @@ import time
 from selenium.webdriver.common.action_chains import ActionChains
 
 # from access_keys import username, password
-from excelReader import credentials
-account =('https://apps.twitter.com/')
+# from excelReader import credentials
+import pandas as pd
+file_name = "email_credentials.xlsx"
+excel_data = pd.read_excel(file_name)
+
+
+
 '''
 Use this in case of windows.
 '''
@@ -37,87 +42,46 @@ In case of mac.
 # signIn.click()
 # print("Twitter app opened")
 
-
-
-for username in credentials:
-	driver = webdriver.Chrome(executable_path = DRIVER_BIN)
+# issues = ['not active', 'active', 'active', 'not active', 'active', 'active', 'active', 'active', 'not active']
+issues = []
+account =('https://apps.twitter.com/')
+for username, password in zip(excel_data.username, excel_data.password):
+   # driver = webdriver.Chrome(executable_path = DRIVER_BIN)
 	# driver = webdriver.Chrome(r'C:\testDir\chromedriver_win32\chromedriver.exe', chrome_options=chrome_options)
-	time.sleep(3)
-	driver.get(account)
-	signIn = driver.find_element_by_xpath('//a[@href="https://twitter.com/login?redirect_after_login=https%3A//apps.twitter.com/"]');
-	signIn.click()
-	print("Twitter app opened")
+   time.sleep(1)
+   driver.get(account)
+   signIn = driver.find_element_by_xpath('//a[@href="https://twitter.com/login?redirect_after_login=https%3A//apps.twitter.com/"]');
+   signIn.click()
+   print("Twitter app opened")
 
-	email = driver.switch_to_active_element()
-	time.sleep(3)
+   email = driver.switch_to_active_element()
+   time.sleep(1)
 	# email = driver.find_element_by_class_name("swift-loading no-nav-banners")
-	email.send_keys(username)
-	email.send_keys(Keys.TAB)
-	print("email entered")
+   email.send_keys(username)
+   email.send_keys(Keys.TAB)
+   print("email entered")
 
-	time.sleep(3)
-	email = driver.switch_to_active_element()
+   time.sleep(1)
+   email = driver.switch_to_active_element()
 	# passcode = driver.find_element_by_name("session[password]")
-	email.send_keys(credentials[username])
-	email.send_keys(Keys.RETURN)
-	print("password entered")
+   email.send_keys(password)
+   email.send_keys(Keys.RETURN)
+   print("password entered")
 
-
-	time.sleep(2)
+   time.sleep(2)
 	# LogIn = driver.find_element_by_xpath('//input[@type="submit"]')
 	# LogIn.click()
-	print("Logged in")
+   print("Logged in")
+#    print("URL:" + driver.current_url)
+   if driver.current_url == 'https://apps.twitter.com/':
+       issues.append("active")
+   else:
+       issues.append("not active")
+#    issues.append(driver.current_url)
 
-	driver.close()
-
-# New_app = driver.find_element_by_xpath("//a[@href ='/app/new']")
-# New_app.send_keys(Keys.RETURN)
-
-# name = driver.find_element_by_name("name")
-# name.send_keys("John Abraham")
-# name.send_keys(Keys.TAB)
-
-# description = driver.switch_to_active_element()
-# description.send_keys("Force and Rocky handsome")
-# description.send_keys(Keys.TAB)
-
-# website = driver.switch_to_active_element()
-# website.send_keys("https://www.google.com")
-# time.sleep(1)
-
-# website.send_keys(Keys.TAB)
-# time.sleep(1)
-
-# Oauth = driver.switch_to_active_element()
-# Oauth.send_keys(Keys.TAB)
-# time.sleep(1)
-
-# URL = driver.switch_to_active_element()
-# URL.send_keys(Keys.TAB)
-# time.sleep(1)
-
-# URL_Tab = driver.switch_to_active_element()
-# URL_Tab.send_keys(Keys.TAB)
-# time.sleep(1)
-
-# Confirm = driver.switch_to_active_element()
-# Confirm.send_keys(Keys.SPACE)
-# Confirm.send_keys(Keys.TAB)
-# time.sleep(1)
-
-# link = driver.switch_to_active_element()
-# link.send_keys(Keys.TAB)
-# time.sleep(1)
-
-
-# Create = driver.switch_to_active_element()
-# Create.send_keys(Keys.RETURN)
-# print(driver.current_url)
-# Akmal_app = driver.find_element_by_xpath("//a[@href ='app/15260788/show']").click()
-# time.sleep(5)
-# driver.find_element_by_xpath('//div[contains(text(), "Keys and Access Tokens")]').click()
-# driver.get(driver.current_url + "/keys")
-# # page = (driver.page_source)
+   driver.close()
 #
-# get_access = driver.find_element_by_name("op")
-# get_access.click()
+excel_data['issues'] = issues
+print(issues)
+excel_data.to_excel(file_name)
+print(excel_data)
