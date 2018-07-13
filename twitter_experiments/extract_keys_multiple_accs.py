e2f9047285eba5c11abed740d003f6feee772cc3
merethrond <merethrond@gmail.com>
2018-07-13 20:10:10 +0530

    collect keys from already made apps and login and wait functions

--- twitter_experiments/extract_keys_multiple_accs.py
+++ twitter_experiments/extract_keys_multiple_accs.py
@@ -5,8 +5,9 @@ Created on Mon Jul  9 16:13:38 2018
 
 @author: tuffy
 """
-# Abhishek
 import os
+import time
+from pandas import read_excel
 from selenium import webdriver
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 DRIVER_BIN = os.path.join(PROJECT_ROOT, "/Users/tuffy/Desktop/pr/Chromedriver")
@@ -31,5 +32,20 @@ def delete_multiple_apps():
         login_to_twitter(driver, username, credentials[username])
         delete_first_app(driver, username)
         driver.close()
+
+def collect_keys_multiple_apps():
+    for username in credentials.keys():
+        driver = webdriver.Chrome(executable_path = DRIVER_BIN)
+        login_to_twitter(driver, username, credentials[username])
+        to_excel(get_keys_of_first_app(driver), username)
+        driver.close()
+def login_and_wait():
+    df = read_excel("filename.xlsx")
+    for username in df.username:
+        driver = webdriver.Chrome(executable_path = DRIVER_BIN)
+        login_to_twitter(driver, username, credentials[username])
+        time.sleep(10)
 # delete_multiple_apps()
 # create_apps_save_keys()
+login_and_wait()
+# collect_keys_multiple_apps()
