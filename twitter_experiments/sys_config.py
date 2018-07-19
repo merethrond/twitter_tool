b79b90ac0cc8a68f6f19e61f072e5940c7d2f052
merethrond <merethrond@gmail.com>
2018-07-19 13:11:01 +0530

    call functions inside the config file

--- twitter_experiments/sys_config.py
+++ twitter_experiments/sys_config.py
@@ -1,13 +1,14 @@
-from selenium import webdriver 
+from selenium import webdriver
 
 chrome_options = webdriver.ChromeOptions()
 prefs = {"profile.default_content_setting_values.notifications" : 2}
 chrome_options.add_experimental_option("prefs",prefs)
 
-def mac_abhishek(): 
-	import os.path
-	PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-	path = os.path.join(PROJECT_ROOT, "/Users/tuffy/Desktop/pr/Chromedriver")
+def mac_abhishek():
+    import os.path
+    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+    path = os.path.join(PROJECT_ROOT, "/Users/tuffy/Desktop/pr/Chromedriver")
+    return path
 	# mac_config = DRIVER_BIN
 
 def pc_ayush():
@@ -15,3 +16,4 @@ def pc_ayush():
 	return path
 
 # driver = webdriver.Chrome(r'C:\testDir\chromedriver_win32\chromedriver.exe', chrome_options=chrome_options)
+path = mac_abhishek()
--- twitter_experiments/testDriver.py
+++ twitter_experiments/testDriver.py
@@ -1,4 +1,3 @@
 from sys_config import *
-path = pc_ayush()
 driver = webdriver.Chrome(path,chrome_options = chrome_options)
-
+driver.close()
