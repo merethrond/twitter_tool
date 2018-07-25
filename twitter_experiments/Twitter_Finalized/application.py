36e407705fd0dee5033879b87b325b9091646e34
merethrond <merethrond@gmail.com>
2018-07-25 16:37:32 +0530

    change

--- twitter_experiments/Twitter_Finalized/application.py
+++ twitter_experiments/Twitter_Finalized/application.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from api_action_lib import api_dict_creation, user_keys_dataframe, update_same_status
+from api_action_lib import api_dict_creation, user_keys_dataframe, update_same_status, like_from_id, retweet_from_id
 app = Flask(__name__)
 
 api_dict = api_dict_creation(user_keys_dataframe)
@@ -11,15 +11,23 @@ def index():
 
 @app.route("/hello", methods=["POST"])
 def hello():
-    twitter_id = request.form.get("tweet_id")
+    twitter_id = int(request.form.get("tweet_id"))
     print("retweet checkbox is:", request.form.get("retweet_check"))
     print("like checkbox is:", request.form.get("like_check"))
     print("Twitter ID is:", twitter_id)
+    wait_interval = int(request.form.get("wait_interval"))
+
     print(username_list)
+    if request.form.get("like_check") == 'on':
+        like_from_id(api_dict, twitter_id, wait_interval)
+    if request.form.get("retweet_check") == 'on':
+        retweet_from_id(api_dict, twitter_id, wait_interval)
+
     return render_template("hello.html", name=twitter_id)
 
 @app.route("/tweet", methods = ["POST"])
 def tweet_bomb():
     tweet_text = request.form.get("tweet_text")
-    update_same_status(tweet_text, api_dict)
+    wait_interval = int(request.form.get("wait_interval"))
+    update_same_status(api_dict, tweet_text, wait_interval)
     return "Tweeted"
