1cfe5b41ef2ec159d178845e71ad9816818b2efd
merethrond <merethrond@gmail.com>
2018-07-31 13:51:08 +0530

    Add chromedriver file and replace logger with print to speed things up

--- twitter_experiments/Twitter_Finalized/check_login_status.py
+++ twitter_experiments/Twitter_Finalized/check_login_status.py
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 excel_data = pd.read_excel(login_excel)
 
 def login(driver, username, password):
-#NEED TO CHECK IF CONNECTION IS ACTIVE OR NOT! 
+#NEED TO CHECK IF CONNECTION IS ACTIVE OR NOT!
     time.sleep(1)
     try:
         driver.get('https://apps.twitter.com/')
@@ -26,57 +26,57 @@ def login(driver, username, password):
         logger.error("NOT CONNECTED TO THE INTERNET!") ###NEEDS IMPROVEMENT
         return '404'
     # print("Opening twitter")
-    logger.info('apps.twitter.com opened')
+    print('apps.twitter.com opened')
     signIn = driver.find_element_by_xpath('//a[@href="https://twitter.com/login?redirect_after_login=https%3A//apps.twitter.com/"]');
     signIn.click()
-    logger.info("Username:%s", username)
+    print("Username:%s", username)
 
     email = driver.switch_to_active_element()#Maybe we will select element instead of this.
     # print(email.get_attribute('value'))
     # if(email.text == username):
     #   print('entered correctly')
     # else:
-    #   print('incorrectly entered')  
+    #   print('incorrectly entered')
     time.sleep(3)
     email.send_keys(username)
     email.send_keys(Keys.TAB)
-    logger.info("email entered")
+    print("email entered")
 
     time.sleep(1)
     email = driver.switch_to_active_element()
     email.send_keys(password)
     email.send_keys(Keys.RETURN)
-    logger.info("password entered")
+    print("password entered")
 
     time.sleep(2)
     # print(driver.current_url)
-    logger.info(driver.current_url)
+    print(driver.current_url)
     if(driver.current_url != 'https://apps.twitter.com/'):
       logger.error('Not an Active Account!')
       return False
 
-    logger.info("Logged in successfully with username:%s",username)
+    print("Logged in successfully with username:%s",username)
     return True
 
 def read_login_credentials():
     issues = []
     for username, password in zip(excel_data.username, excel_data.password):
         driver = webdriver.Chrome(executable_path = path)
-        logger.info("Chrome Driver Initiated")
-        
+        print("Chrome Driver Initiated")
+
         for i in range(3):
-            logger.info("Attempt #%i",i)
-            status = login(driver, username, password)    
+            print("Attempt #%i",i)
+            status = login(driver, username, password)
             if (status):
             # Will check through app creation later.
-                logger.info("changing status to active")
+                print("changing status to active")
                 issues.append("active")
                 break
             elif status == '404':
                 logger.warning("status code 404 received, Stopping read_login_credentials execution!")
                 break
             else:
-                logger.info("changing status to not active")
+                print("changing status to not active")
                 issues.append("not active")
             # print("Logged in")
         # else:
@@ -85,13 +85,13 @@ def read_login_credentials():
         #       print("Logged in")
         #     else:
         #       issues.append("not active")
-        logger.info("Closing Chrome Driver")
+        print("Closing Chrome Driver")
         driver.close()
     excel_data['issues'] = issues
     excel_data.to_excel(login_excel)
 
 def convert_to_dictionary():
-    logger.info("Converting active users to credential dictionary")
+    print("Converting active users to credential dictionary")
     return {i:j for i, j, k in zip(excel_data.username, excel_data.password, excel_data.issues) if k == 'active'}
 
 # read_login_credentials()
